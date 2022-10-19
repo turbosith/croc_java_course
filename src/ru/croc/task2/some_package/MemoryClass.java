78a0340333d6cc214b39fa329fabe3539e81a1b6
turbosith <93432684+turbosith@users.noreply.github.com>
2022-10-20 01:32:35 +0300

    Optimized the task2

--- src/ru/croc/task2/some_package/MemoryClass.java
+++ src/ru/croc/task2/some_package/MemoryClass.java
@@ -2,23 +2,20 @@ package ru.croc.task2.some_package;
 
 public class MemoryClass {
     private double bytesNumber;
+    String[] dataTypesArray = {"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};//массив типов данных
 
     public MemoryClass(double bytesNum) {//конструктор класса, получает количество байт и записывает в атрибут
         bytesNumber = bytesNum;
     }
 
     public void format() {//метод, форматирующий и выводящий на экран заданный размер в байтах в человекочитаемом виде
-        if (bytesNumber < 1024) {
-            System.out.println(String.format("%.1f", bytesNumber) + " B");
-        } else if (bytesNumber < 1048576) {
-            System.out.println(String.format("%.1f", bytesNumber / 1024) + " KB");
-        } else if (bytesNumber < 1073741824) {
-            System.out.println( String.format("%.1f", bytesNumber / 1048576) + " MB");
-        } else if (bytesNumber < 1099511627776L) {
-            System.out.println( String.format("%.1f", bytesNumber / 1073741824) + " GB");
-        } else {
-            System.out.println( String.format("%.1f", bytesNumber / 1099511627776L) + " TB");
+        int numberDivisions = 0;//количество делений на 1024
+        while (bytesNumber / 1024 >= 1) {//проверяем, сколько раз число делится на 1024
+            numberDivisions += 1;
+            bytesNumber /= 1024;
         }
+        System.out.println(String.format("%.1f", bytesNumber) + " " + dataTypesArray[numberDivisions]);//сопоставляем тип данных из массива
+
 
     }
 }
