e5742cf34aa36cd5f0bcdbcd6e09c5889357f62f
Данила <93432684+turbosith@users.noreply.github.com>
2022-11-20 02:11:15 +0300

    Fixed Task 11, added check synchronization and eliminated unnecessary data caching

--- src/ru/croc/task11/lot/Lot.java
+++ src/ru/croc/task11/lot/Lot.java
@@ -3,32 +3,33 @@ package ru.croc.task11.lot;
 import java.time.LocalTime;
 
 public class Lot {
-    float currentValue;
-    String username;
-    LocalTime endTimeTrading;
+    private volatile float currentValue;
+    private volatile String username;
+    private final LocalTime endTimeTrading;
 
     private static final Object lock = new Object();
 
     /**
      * Создаёт новый {@code Lot}
      *
-     * @param currentValue   - начальная стоимость
+     * @param startValue     - начальная стоимость
      * @param endTimeTrading - время окончнания торгов
      */
-    public Lot(float currentValue, LocalTime endTimeTrading) {
+    public Lot(float startValue, LocalTime endTimeTrading) {
+        this.currentValue = startValue;
         this.endTimeTrading = endTimeTrading;
     }
 
     public void placeBet(int newValue, String newUsername) {
 
         LocalTime time = LocalTime.now();
-        if ((newValue > currentValue) && (time.isBefore(endTimeTrading))) {
-            synchronized (lock) { //синхронизация позволяет добиться потокобезопасности метода
+        synchronized (lock) {
+            if ((newValue > currentValue) && (time.isBefore(endTimeTrading))) {
+                //синхронизация позволяет добиться потокобезопасности метода
                 currentValue = newValue;// чтобы пользователи могли одновременно делать ставка
                 username = newUsername;
             }
-        } else {
-            System.out.println("Ставка не может быть совершена");
+
         }
 
     }
