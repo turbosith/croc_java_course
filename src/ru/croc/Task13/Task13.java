2c8ff3d10a214e766b53675c49441dd78142d0dc
turbosith <93432684+turbosith@users.noreply.github.com>
2022-11-23 14:46:49 +0300

    add file read

--- src/ru/croc/Task13/Task13.java
+++ src/ru/croc/Task13/Task13.java
@@ -1,22 +1,29 @@
 package ru.croc.Task13;
 
+import java.io.*;
 import java.util.*;
 
 public class Task13 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  IOException,NoSuchElementException,IllegalStateException {
         Map<Integer, String> films = new HashMap<>();
-        films.put(1, "Мстители: Финал");
-        films.put(2, "Хатико");
-        films.put(3, "Дюна");
-        films.put(4, "Унесенные призраками");
-        int[] user;
+        try (Scanner s = new Scanner(new FileReader("films.txt"))) {
+            while (s.hasNextLine()){
+                String scan=s.nextLine();
+                films.put(scan.charAt(0)-'0',scan.substring(2));}
+        }
         List<int[]> users = new ArrayList<>();
-        user = new int[]{2, 1, 3};
-        users.add(user);
-        user = new int[]{1, 4, 3};
-        users.add(user);
-        user = new int[]{2, 2, 2, 2, 2, 3};
-        users.add(user);
+        try (Scanner s = new Scanner(new FileReader("users.txt"))) {
+            while (s.hasNextLine()) {
+                int iterationCount = 0;
+                String scan = s.nextLine();
+                int[] user = new int[scan.length() / 2 + 1];
+                for (int i = 0; i < scan.length(); i += 2, iterationCount += 1) {
+                    user[iterationCount]=scan.charAt(i) - '0';
+                }
+                users.add(user);
+            }
+
+        }
         List<Integer> movies = new ArrayList<>();
         int iterationCount = 0;
         Scanner s = new Scanner(System.in);
