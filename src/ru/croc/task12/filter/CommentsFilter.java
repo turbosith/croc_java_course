f77784a5f8e76b08dab64716f6530a62a60aa6a7
turbosith <93432684+turbosith@users.noreply.github.com>
2022-11-24 20:36:04 +0300

    simplified the method of searching for bad words

--- src/ru/croc/task12/filter/CommentsFilter.java
+++ src/ru/croc/task12/filter/CommentsFilter.java
@@ -17,30 +17,23 @@ public class CommentsFilter implements BlackListFilter {
         List<String> badComments = new ArrayList<>();//список плохих комментариев
         int asciiFrontChar = 0;//ASCII код предыдущего перед словом символа
         int asciiBackChar = 0;//ASCII последующего символа
-
         for (String comment : comments) {
             for (String blackWord : blackList) {
-                //плохое слово без первого сивола(понадобиться для исключения плохих слов с заглавной буквы)
-                String blackWordWithoutFirstSymbol = blackWord.substring(1);
-
-                if (comment.contains(blackWordWithoutFirstSymbol)) {//проверка содержит ли комментарий плохое слово
-
-
-                    int blackWordWithoutFirstSymbolIndex = comment.indexOf(blackWordWithoutFirstSymbol);
+                //делаем все буквы строчными, чтобы затронуть плохие слова с заглавной буквы
+                String CommentLowerCase=comment.toLowerCase();
+                int blackWordIndex=CommentLowerCase.indexOf(blackWord);//индекс плохого слова
+                if (blackWordIndex!=-1) {//проверка содержит ли комментарий плохое слово
                     //проверка, можем ли мы получить предыдущий и последующий символы
-                    if (blackWordWithoutFirstSymbolIndex != 1) {
-                        asciiFrontChar = comment.charAt(blackWordWithoutFirstSymbolIndex - 2);
+                    if (blackWordIndex != 0) {
+                        asciiFrontChar = CommentLowerCase.charAt(blackWordIndex - 1);
                     }
-                    if (blackWordWithoutFirstSymbolIndex + blackWordWithoutFirstSymbol.length() != comment.length() + 1) {
-                        asciiBackChar = comment.charAt(blackWordWithoutFirstSymbolIndex +
-                                blackWordWithoutFirstSymbol.length());
+                    if (blackWordIndex + blackWord.length() != CommentLowerCase.length() ) {
+                        asciiBackChar = CommentLowerCase.charAt(blackWordIndex +
+                                blackWord.length());
                     }
-
-                    //проверка, не содержится ли наше слово с другом и не начинается ли оно с заглавной буквы
-                    if (!(isCharLetter(asciiFrontChar)) &
-                            !(isCharLetter(asciiBackChar)) &
-                            ((int) blackWord.charAt(0) !=
-                                    comment.indexOf(blackWordWithoutFirstSymbolIndex) + 31)) {
+                    if (!(isCharLetter(asciiFrontChar)) & //проверка, не содержится ли наше слово в другом
+                            !(isCharLetter(asciiBackChar)))
+                    {
                         badComments.add(comment);//добавление в список плохих комментариев
                     }
                 }
@@ -48,7 +41,6 @@ public class CommentsFilter implements BlackListFilter {
         }
         comments.removeAll(badComments);//удаление всех плохих комментариев
     }
-
     /**
      * Проверяет буква сивол или нет(работает на английском и русском языках)
      *
@@ -56,8 +48,7 @@ public class CommentsFilter implements BlackListFilter {
      * @return буква это или нет
      */
     public boolean isCharLetter(int asciiChar) {
-        return (asciiChar > 64 && asciiChar < 91) |
-                (asciiChar > 96 && asciiChar < 123) |
+        return (asciiChar > 96 && asciiChar < 123) |
                 (asciiChar > 1039 && asciiChar < 1104);
     }
 }
