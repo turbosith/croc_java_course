735de1f733a535b9f505e87a6c6756d0945127d9
Данила <danila.evdokimov.444@mail.ru>
2022-12-07 06:18:40 +0300

    Initial commit Task17

---
+++ src/ru/croc/task17/Task17.java
@@ -0,0 +1,16 @@
+package ru.croc.task17;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Task17 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+
+    }
+}
