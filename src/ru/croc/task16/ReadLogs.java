620d780a31d1a15791db4800c4cf74ffa386085e
Данила <danila.evdokimov.444@mail.ru>
2022-12-08 08:44:54 +0300

    fixed Task16

--- src/ru/croc/task16/ReadLogs.java
+++ src/ru/croc/task16/ReadLogs.java
@@ -3,10 +3,16 @@ package ru.croc.task16;
 import java.io.*;
 import java.nio.file.Path;
 import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Scanner;
+
 
 public class ReadLogs {
+
+
+    private final List<Log> arrayLog = new ArrayList<>();
+
+    private final List<BufferedReader> readers = new ArrayList<>();
 
 
     /**
@@ -15,8 +21,8 @@ public class ReadLogs {
      * @param path - путь к директории с файлами
      */
     public void readFilesFromDirectory(Path path) throws IOException {
-        File finalLogFile = new File("Task16/logs/final_log.log");
-        BufferedWriter writer = new BufferedWriter(new FileWriter(finalLogFile, true));
+
+
         File directory = new File(path.toUri()); //path указывает на директорию
         File[] folderEntries = directory.listFiles();
         assert folderEntries != null;
@@ -27,15 +33,7 @@ public class ReadLogs {
                 if (entry.isFile() && ((entry.getName().endsWith(".log")) |
                         (entry.getName().endsWith(".trace")) | (entry.getName().endsWith(".LOG")))) {
 
-                    try (Scanner s = new Scanner(new FileReader(entry))) {
-                        while (s.hasNextLine()) {
-                            String scan = s.nextLine();
-
-                            writer.append(scan).append(" ");//запись лога в буфер, чтобы не тратить оперативную память
-
-
-                        }
-                    }
+                    firstBypass(entry);//записываем первичные данные файла
 
 
                 }
@@ -43,38 +41,88 @@ public class ReadLogs {
             }
 
         }
-        writer.close();//закрывание буфера
+
 
     }
 
 
     /**
-     * Считывание логов из буфера
+     * Вывод логов в отсортированном порядке
      *
-     * @return - список логов
-     * @throws FileNotFoundException - ошибка не нахождения файла
+     * @throws IOException - ошибка ввода вывода
      */
-    public List<Log> readLogs() throws FileNotFoundException {
-        List<Log> logList = new ArrayList<>();
-        File logFile = new File("Task16/logs/final_log.log");
+    public void outLogs() throws IOException {
+        long maxTime = 1969778144951L;//максимальное время, для инициализации минимума
 
-        try (Scanner s = new Scanner(new FileReader(logFile))) {
 
-            String scan = s.nextLine();
-            String[] logParts = scan.split(" ");
-            for (int i = 0; i < logParts.length - 1; i++) {
-                Log log = new Log(Long.parseLong(logParts[i]), logParts[i + 1]);
-                logList.add(log);
-                i += 1;
+        LogComparator logComparator = new LogComparator();
+
+        int indexMinLog = 0;//индекс самого раннего лога
+        boolean flag = true;
+        while (flag) {
+            Log minLog = new Log(maxTime, "dfds");//инициализация лога, для нахождения минимального
+            flag = false;
+            for (int j = 0; j < arrayLog.size(); j++) {
+                if (arrayLog.get(j) != null) {
+                    flag = true;
+                    if (arrayLog.get(j) != null && logComparator.compare(arrayLog.get(j), minLog) < 0) {
+
+                        minLog = arrayLog.get(j);
+                        indexMinLog = j;
+
+
+                    }
+                }
+
             }
+            if (minLog.time() != maxTime) {
+                System.out.println(minLog.time() + " " + minLog.message());
+                arrayLog.set(indexMinLog, readLog(readers.get(indexMinLog)));
+            }
+        }
 
+
+    }
+
+    /**
+     * Считывание лога
+     *
+     * @param bufferedReader - ридер, из которого мы должны получить следующую строку
+     * @return - считанный лог
+     */
+    Log readLog(BufferedReader bufferedReader) throws IOException {
+        String scan = bufferedReader.readLine();
+        if (scan != null) {
+            String[] logParts = scan.split(" ");
+
+            return new Log(Long.parseLong(logParts[0]), logParts[1]);
+        }
+        return null;
+
+
+    }
+
+    /**
+     * Первое прохождение по файлу, запись первой строки в массив и запись ридера
+     *
+     * @param file - файл, который нужно записать
+     */
+    private void firstBypass(File file) throws IOException {
+
+        String line;
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+        if ((line = bufferedReader.readLine()) != null) {
+
+
+            String[] logParts = line.split(" ");
+            Log log = new Log(Long.parseLong(logParts[0]), logParts[1]);
+            arrayLog.add(log);
+            readers.add(bufferedReader);
 
         }
-        PrintWriter writer = new PrintWriter(logFile);//очистка буфера
-        writer.print("");
-        writer.close();
-
-        return logList;
     }
+
 
 }
--- src/ru/croc/task16/Task16.java
+++ src/ru/croc/task16/Task16.java
@@ -10,11 +10,7 @@ public class Task16 {
         ReadLogs readLogs = new ReadLogs();
         String pathText = args[0];//ввод пути из консоли
         readLogs.readFilesFromDirectory(Path.of(pathText));
-        List<Log> logs = readLogs.readLogs(); // /Users/79165/Desktop/croc/java-school-2022-croc/src/ru/croc/task16/logs
-        logs.sort(new LogComparator());
-        for (Log log : logs) { //вывод времени и сообщений логов
-            System.out.println(log.time() + " " + log.message());
-        }
+        readLogs.outLogs(); // /Users/79165/Desktop/croc/java-school-2022-croc/src/ru/croc/task16/logs
 
     }
 }
